f967fd34147486b880d1281471f3f83337f44985
Jeremy Thorne <jez@jezandbecks.co.uk>
2022-01-24 21:18:19 +0000

    removed whitespace breaking wikipedia puzzle
    realised whitespace at start of lines in heredoc of wikipedia puzzle was
    causing an underspecified board leading to very long (if ever) solve times.
    
    now solves quickly without issue.

--- sudoku.py
+++ sudoku.py
@@ -223,14 +223,14 @@ def wikipedia() -> List[int]:
     # this is quite a hard puzzle
     return parse_str(
     '''53  7
-       6  195
-        98    6
-       8   6   3
-       4  8 3  1
-       7   2   6
-        6    28
-          419  5
-           8  79''')
+6  195
+ 98    6
+8   6   3
+4  8 3  1
+7   2   6
+ 6    28
+   419  5
+    8  79''')
 
 
 def parse_str(puzzle:str) -> Optional[List[int]]:
